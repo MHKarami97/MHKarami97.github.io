c0c08156e0e11ba0364b56ed2dd3ba05bb7fd0ee
mhkarami97 <mhkarami97@gmail.com>
2024-03-05 10:50:13 +0330

    fix bug

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -235,7 +235,7 @@ const config = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made By <a 
-      class="text-secondary" href="https://github.com/mhkarami97"
+      class="text-primary" href="https://github.com/mhkarami97"
       target="_blank"
       rel="noreferrer"
     >Mohammad Hossein Karami</a>`,
